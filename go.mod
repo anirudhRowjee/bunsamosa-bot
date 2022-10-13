2e7216ccc2a7f97c7f729819005ba88e2b6a2455
Anirudh Rowjee <ani.rowjee@gmail.com>
2022-10-13 22:11:09 +0530

    Added basic scaffolding for Github bot

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/anirudhRowjee/bunsamosa-bot
 
 go 1.18
+
+require (
+	github.com/bradleyfalzon/ghinstallation/v2 v2.1.0 // indirect
+	github.com/go-playground/webhooks/v6 v6.0.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/google/go-github/v45 v45.2.0 // indirect
+	github.com/google/go-github/v47 v47.1.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"github.com/bradleyfalzon/ghinstallation/v2"
+	// v3 "github.com/google/go-github/v47/github"
+	// ghwebhooks "github.com/go-playground/webhooks/v6"
+	"gopkg.in/yaml.v2"
+
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type App struct {
+	webhookSecret  string
+	appID          int64
+	orgID          string
+	certPath       string
+	installationID int64
+	itr            *ghinstallation.Transport
+	// `itr` looks to be InstallationTransport
+}
+
+func (a *App) parse_from_YAML(path string) {
+
+	// TODO Parse these credentials from YAML
+	filename, _ := filepath.Abs(path)
+	yamlFile, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, a)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Parsed", a)
+}
 
 func main() {
+
+	// parse YAML File to read in secrets
+
+	myapp := App{}
+	myapp.parse_from_YAML("./secrets.yaml")
 	fmt.Println("Hello, world!")
 }
